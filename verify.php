cdf196a46dc53dcb0e51828ed22ebb984d683ee7
niaekralc <grahamrclarke@gmail.com>
2013-05-02 20:34:29 +0200

    Now includes visitor sign in.

--- verify.php
+++ verify.php
@@ -1,42 +1,39 @@
 <?php 
 session_start(); 
+require 'DBconnection.php';
 $_POST["pwd"] = md5($_POST["pwd"]); 
 $_SESSION['user']=$_POST["user"];
-$_SESSION['level']=1;
 ?>
-
 <html>
-
 <head>
-
 <?php
-  	$verification = FALSE;
-// Create connection
-$_SESSION['con']=mysqli_connect("localhost","root","","uni_database");
-$con=$_SESSION['con'];
+		$verification = FALSE;
+if ( isset($_GET["visitor"]) ) 
+{
+	$_SESSION['level'] = 1;
+	$verification = TRUE;
+}
 
-// Check connection
 if (!$con) {
-echo "<meta http-equiv=\"refresh\" content=\"0;URL='login.php?message=Failed to connect to database - contact administrator.'\">";
-}
-else{
+	echo "<meta http-equiv=\"refresh\" content=\"0;URL='login.php?message=Failed to connect to database - contact administrator.'\">";
+} else{
 				// Everything else here!!
-$result = mysqli_query($con, "SELECT Username, Password FROM Users");
-while($row = mysqli_fetch_array($result))
-	{
-		if ($row['Username'] == $_POST["user"] && $row['Password'] == $_POST["pwd"])
+	$result = mysqli_query($con, "SELECT Username, Password, User_Level FROM Users");
+	while($row = mysqli_fetch_array($result))
 		{
-			$verification = TRUE;
+			if ($row['Username'] == $_POST["user"] && $row['Password'] == $_POST["pwd"])
+			{
+				$verification = TRUE;
+				$_SESSION['level'] = $row['User_Level'];
+				$_SESSION['ID'] = $row['P_Id'];
+			}
 		}
+	
+	if($verification){
+		echo "<meta http-equiv=\"refresh\" content=\"0;URL='search.php'\">";
+	}else {
+		echo "<meta http-equiv=\"refresh\" content=\"0;URL='login.php?message=Incorrect user name or password!'\">";
 	}
-
-
-
-if($verification){
-echo "<meta http-equiv=\"refresh\" content=\"0;URL='search.php'\">";
-}else {
-echo "<meta http-equiv=\"refresh\" content=\"0;URL='login.php?message=Incorrect user name or password!'\">";
-}
 }
 ?>
 </head>

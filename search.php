ca94521c7c61317df39e840dc3359786c603f91c
niaekralc <grahamrclarke@gmail.com>
2013-05-02 20:31:47 +0200

    Update search.php

--- search.php
+++ search.php
@@ -1,30 +1,27 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-<link rel="stylesheet" type="text/css" href="styles.css">
-<!--external link to change the colors,size, and anything that has to do with style-->
-</head>
-<body>
-<table border="0" cellpadding= "10", align= "center">
-<tr>
-<td><h4><a href="http://www.youtube.com/watch?v=lRRUxAccoN0" target="_blank">Administrator</a></h4></td>
-<td><h4><a href="http://www.youtube.com/watch?v=lRRUxAccoN0" target="_blank">Browse</a></h4></td>
-<td><h4><a href="http://www.youtube.com/watch?v=lRRUxAccoN0" target="_blank">Reports</a></h4></td>
-<td><h4><a href="http://www.youtube.com/watch?v=lRRUxAccoN0" target="_blank">Profile</a></h4></td>
-<td><h4><a href="http://www.youtube.com/watch?v=lRRUxAccoN0" target="_blank">Contact us</a></h></td>
-<td><h4><a href="http://www.youtube.com/watch?v=lRRUxAccoN0" target="_blank">Sign-out</a></h4></td>
-</tr>
-</table> 
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<h3> Our Research Database </h1>
-<br>
+<?php
+require 'session.php';
+require 'DBconnection.php';
+if ( isset($_SESSION['level']) ):
+   ?>
+	<!DOCTYPE html>
+	<html>
+	<head>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<!--external link to change the colors,size, and anything that has to do with style-->
+	
+	</head>
+<?php include 'menu.php';?>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<h3> Research Database </h1>
+	<br>
+	
+<!-- 	SEARCH FORM    -->
 <form name="input" action="search_results.php" method="GET">
 <h2>Search: <input type="text" name="search_value" size="90" id="textField" style="background-image:url(filename.jpg)"/>
 <select name="filter">
@@ -35,5 +32,11 @@
 <br>
 <h2><input type="submit" value="Submit" size= "20"></h2>
 </form> 
-</body>
-</html>
+
+
+<?php else : 
+	echo "<meta http-equiv=\"refresh\" content=\"0;URL='login.php'\">";
+endif; ?>
+
+	 	</body>
+	</html>

71f96d746b9259c34480449e9e86a9efcb4912bc
niaekralc <grahamrclarke@gmail.com>
2013-05-02 15:24:42 +0200

    Create DBconnection.php
    'require' this file in any page that needs a database connection.

---
+++ DBconnection.php
@@ -0,0 +1,9 @@
+  <?php
+	// Create connection
+ 	$con=mysqli_connect("localhost","root","","uni_database");
+	// Check connection
+	if (mysqli_connect_errno($con))
+	{
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	?>

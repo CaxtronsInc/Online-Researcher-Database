e4dd4237444c9e2dc65146d3f99d00b3d6ec2dde
incognitor <ys00082@surrey.ac.uk>
2013-05-09 14:02:13 +0300

    Update search_results.php

--- search_results.php
+++ search_results.php
@@ -1,4 +1,3 @@
-<?php require 'session.php' ?>
 <!DOCTYPE html>
 <html>
 
@@ -7,9 +6,22 @@
 <!--external link to change the colors,size, and anything that has to do with style-->
 </head>
 <body>
+<h1> Website</h1>
+
+<table border="0" cellpadding= "10", align= "center">
+<tr>
+<td><h4><a href="register_user.php" target="_self">Register</a></h4></td>
+<td><h4><a href="http://www.youtube.com/watch?v=lRRUxAccoN0" target="_blank">Administrator</a></h4></td>
+<td><h4><a href="http://www.youtube.com/watch?v=lRRUxAccoN0" target="_blank">Browse</a></h4></td>
+<td><h4><a href="http://www.youtube.com/watch?v=lRRUxAccoN0" target="_blank">Reports</a></h4></td>
+<td><h4><a href="http://www.youtube.com/watch?v=lRRUxAccoN0" target="_blank">Profile</a></h4></td>
+<td><h4><a href="http://www.youtube.com/watch?v=lRRUxAccoN0" target="_blank">Contact us</a></h></td>
+<td><h4><a href="http://www.youtube.com/watch?v=lRRUxAccoN0" target="_blank">Sign-out</a></h4></td>
+</tr>
+</table> 
 <?php
+
 require 'DBconnection.php';
-include 'menu.php';
 
 $noresults = 0;
 
@@ -29,10 +41,9 @@ $filter = $_GET["filter"];
 
 <form name="input" action="search_results.php" method="GET">
 
-<h2>Search: <input type="text" name="search_value" size="90" id="textField" style="background-image:url(filename.jpg)" value = <?php echo $searchvalue ?> >
+<h2>Search: <input type="text" name="search_value" size="90" id="textField" style="background-image:url(filename.jpg)" value = "<?php echo $searchvalue ?>" >
 
 <select name="filter" >
-  
   <option <?php if ($filter == "All Items") echo "selected"; ?> value="AllItems">All Items</option>
   <option <?php if ($filter == "Users") echo "selected"; ?> value="Users">Users</option>
   <option <?php if ($filter == "Publications") echo "selected"; ?> value="Publications">Publications</option>
@@ -44,7 +55,9 @@ $filter = $_GET["filter"];
 
 <?
 
-if (strlen($searchvalue) == 0)
+$searchvalue = trim($searchvalue);
+
+if (strlen($searchvalue) == 0) // if nothing was put in the search engine then refresh the page.
 {
 	echo "<meta http-equiv=\"refresh\" content\0;URL='search_results.php'\">";
 }
@@ -72,7 +85,7 @@ else
 
 		for ($i = 0; $i < count($searchterms); $i++)
 		{
-			$query_str = $query_str . " (Users.First_Name LIKE '%" . $searchterms[$i] . "%' OR Users.Last_Name LIKE '%" . $searchterms[$i] . "%') AND";
+			$query_str = $query_str . " (Users.First_Name LIKE '%" . $searchterms[$i] . "%' OR Users.Last_Name LIKE '%" . $searchterms[$i] . "%') OR";
 		}
 
 		$query_str = substr($query_str,0,count($query_str) - 4);
@@ -91,18 +104,24 @@ else
 			<th>Email</th>
 			</tr>";
 			
-			$address = "https://www.google.co.uk/";
-			$edit_url = "http://cdn1.iconfinder.com/data/icons/fatcow/32/bullet_edit.png";
-			$trash_url = "http://cdn1.iconfinder.com/data/icons/fatcow/32x32_0100/bin_closed.png";
+			
+			$edit_url = "http://cdn1.iconfinder.com/data/icons/fatcow/16/bullet_edit.png";
+			$trash_url = "http://cdn1.iconfinder.com/data/icons/fatcow/16x16_0100/bin_closed.png";
+			$pub_url = "page_add.png";
 		
 			
 			while($row = mysqli_fetch_array($result))
 			{
+				$address = "profile.php?user_id=" . $row['P_Id'];
 				$emailaddress = "mailto:" . $row['Email_Address'] ;
 				$editaddress =  "edit_item.php?edit_item=" . $row['P_Id'];
+				$pubaddress =  "add_publication.php?user_id=" . $row['P_Id'];
+				//edit_item=" . $row['P_Id'];
+				
+				$register_user = "register_user.php?edit_item=" . $row['P_Id'];
 				$deleteaddress = "search_results.php?search_value=" . $searchvalue . "&filter=" . $filter . "&delete_item=" . $row['P_Id'];
 				echo "<tr>";
-				echo "<td><a href=" . $address . " >" . $row['Last_Name'] . ", " . $row['First_Name'] . "</td>" . "<td>" . $row['Office'] . "</td>" . "<td><a href=" . $emailaddress . ">" . $row['Email_Address'] . "</a></td>" . "<td><a href=" . $editaddress . "><img src=" . $edit_url . "></a></td>" . "<td><a href=" . $deleteaddress . " OnClick=\"return confirm('Are you sure?');\"><img src=" . $trash_url . "></a></td>";
+				echo "<td><a href=" . $address . ">" . $row['Last_Name'] . ", " . $row['First_Name'] . "</td>" . "<td>" . $row['Office'] . "</td>" . "<td><a href=" . $emailaddress . ">" . $row['Email_Address'] . "</a></td>" . "<td><a href=" . $register_user . "><img src=" . $edit_url . "></a></td>" . "<td><a href=" . $pubaddress . "><img src=" . $pub_url . "></a></td>" . "<td><a href=" . $deleteaddress . "><img src=" . $trash_url . "></a></td>";
 			}
 			echo "</table>";
 			echo "<br>";
